f77235c8059c5a7e96a772620ebd0b70b7670d09
Josiah Walker <josiah.walker@uon.edu.au>
2015-05-24 05:17:05 +1000

    added gridmap with sonary model

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pyrover
 A simple code-base to start autonomous rover experiments with.
+
+Required libraries:
+- numpy
+- scipy
+- scikit-learn
+- pykalman
---
+++ src/Mapping/gridmap.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 20:47:27 2015
+
+@author: merlz
+"""
+import numpy
+import scipy.sparse
+
+
+#ranges all given in cm
+SonarSensor = {"spread": 15.*numpy.pi/180., "range": 500., "phitfree": -0.2, "phitoccupied": 3.5}
+
+
+def BresenhamLine(A,B):
+    if A[0] == B[0]:
+        if B[1] >= A[1]:
+            return [(A[0],i) for i in xrange(A[1],B[1]+1,1)]
+        else:
+            return [(A[0],i) for i in xrange(A[1],B[1]-1,-1)]
+    flipped = False
+    if (A[0] > B[0]):
+        flipped = True
+        tmp = B
+        B = A
+        A = tmp
+    result = []
+    inc = numpy.sign(B[1]-A[1])
+    error = 0
+    yval = A[1]
+    slope = A-B
+    slope = abs(slope[1]/float(slope[0]))
+    for i in xrange(A[0],B[0]+1):
+        result.append((i,yval))
+        error += slope
+        while error >= 0.5:
+            yval += inc
+            error -= 1.0
+            result.append((i,yval))
+    if flipped:
+        result = result[::-1]
+    return result
+
+#Unlike the line, this does only one pixel per Y row, so it can be used in fill algorithms efficiently
+def BresenhamBorder(A,B):
+    flipped = False
+    if (A[1] > B[1]):
+        flipped = True
+        tmp = B
+        B = A
+        A = tmp
+    result = []
+    xval = A[0]
+    slope = A-B
+    slope = slope[0]/float(slope[1])
+    for i in xrange(A[1],B[1]+1):
+        result.append((int(numpy.round(xval)),i))
+        xval += slope
+    if flipped:
+        result = result[::-1]
+    return result
+
+
+class GridMap:
+    """
+    Generic sparse gridmap.
+    """
+    
+    def __init__(self,size=(100000,100000)):
+        self._map = scipy.sparse.dok_matrix((100000,100000),dtype=numpy.float32)
+    
+    def update(self,position,distance,sensorangle,sensor):
+        """
+        Position is given as (x,y,theta); the sensor field is filled by one of the defines in this file, or similar
+        """
+        thetamax = position[2] + sensor["spread"]/2. + sensorangle
+        thetamin = position[2] - sensor["spread"]/2. + sensorangle
+        
+        #for efficiency, we assume that the object is a straight line, and the area is a triangle
+        A = numpy.array(position[:2])
+        B = numpy.round(numpy.array([numpy.cos(thetamax),numpy.sin(thetamax)])*distance + A).astype(numpy.int32)
+        C = numpy.round(numpy.array([numpy.cos(thetamin),numpy.sin(thetamin)])*distance + A).astype(numpy.int32)
+        A = numpy.round(A).astype(numpy.int32)
+        coords = [A,B,C]
+        
+        #DO FILL OVER THE TRIANGLE ABC
+        #1. sort in Y
+        if A[1] < B[1]:
+            tmp = A
+            A = B
+            B = tmp
+        if A[1] < C[1]:
+            tmp = A
+            A = C
+            C = tmp
+        if B[1] < C[1]:
+            tmp = C
+            C = B
+            B = tmp
+        
+        #2. generate borders
+        emptyVal = sensor["phitfree"]
+        if A[0] == B[0]:
+            beginX = BresenhamBorder(A,C)
+            endX = BresenhamBorder(B,C)
+        else:
+            beginX = BresenhamBorder(A,B)
+            endX = BresenhamBorder(A,C)
+            if len(beginX) < len(endX):
+                beginX += BresenhamBorder(B,C)[1:]
+            elif len(endX) < len(beginX):
+                endX += BresenhamBorder(C,B)[1:]
+        #3. fill the triangle
+        for i in xrange(len(beginX)):
+            if endX[i][0] >= beginX[i][0]:
+                for j in xrange(beginX[i][0],endX[i][0]+1,1):
+                    self._map[j,beginX[i][1]] += emptyVal
+            else:
+                for j in xrange(beginX[i][0],endX[i][0]-1,-1):
+                    self._map[j,beginX[i][1]] += emptyVal
+                
+        
+        #DO BRESENHAM from B to C for hit objects
+        A,B,C = coords
+        hitVals = BresenhamLine(B,C)
+        solidVal = sensor["phitoccupied"]
+        for h in hitVals:
+            self._map[h[0],h[1]] += solidVal
+        
+    
+    def get(self,location):
+        """
+        Locations are [x,y]
+        """
+        location = numpy.round(location).astype(numpy.int32)
+        return self._map(location[0],location[1])
+    
+    def getRange(self,topleft,bottomright):
+        """
+        Locations are [x,y]
+        """
+        result = numpy.zeros((bottomright[0]-topleft[0],bottomright[1]-topleft[1]))
+        for i in xrange(topleft[0],bottomright[0]):
+            for j in xrange(topleft[1],bottomright[1]):
+                result[i-topleft[0],j-topleft[1]] = self._map[i,j]
+        return result
+
+
+
+
+if __name__ == '__main__':
+    """
+    Do validation test
+    """
+    import time
+    scale = 100.0
+    groundtruth = ((1,1,1,1,1),(1,0,0,0,1),(1,0,1,0,1),(1,0,0,0,1),(1,1,1,1,1))
+    estmap = GridMap()
+    
+    tour = ((150.0,150.0,0.0),(350.0,150.0,0.0),
+            (350.0,150.0,-numpy.pi/2.0),(350.0,350.0,-numpy.pi/2.0),
+            (350.0,350.0,-numpy.pi),(150.0,350.0,-numpy.pi),
+            (150.0,350.0,-numpy.pi*1.5),(150.0,150.0,-numpy.pi*1.5),(150.0,150.0,-numpy.pi*2))
+    
+    divs =200
+    vals = []
+    for i in xrange(len(tour)-1):
+        
+        
+        for j in xrange(divs):
+            position = numpy.array(tour[i])*(1.-j/float(divs))+numpy.array(tour[(i+1)%len(tour)])*(j/float(divs))
+            #position[2] += numpy.sin(j/10.)
+            #get range
+            for k in xrange(4):
+                sensor = SonarSensor
+                sensorangle = numpy.pi/2*k
+                thetamax = position[2] + sensor["spread"]/2. + sensorangle
+                thetamin = position[2] - sensor["spread"]/2. + sensorangle
+                baseB = numpy.array([numpy.cos(thetamax),numpy.sin(thetamax)])
+                baseC = numpy.array([numpy.cos(thetamin),numpy.sin(thetamin)])
+                hit = False
+                for distance in xrange(int(sensor["range"])):
+                    B = numpy.round(baseB*distance + position[:2]).astype(numpy.int32)
+                    C = numpy.round(baseC*distance + position[:2]).astype(numpy.int32)
+                    
+                    for pos in BresenhamLine(B,C):
+                        if groundtruth[int((pos[0]/scale))][int((pos[1]/scale))] == 1:
+                            hit = True
+                            break
+                    if hit:
+                        t0 = time.time()
+                        estmap.update(position,distance,sensorangle,sensor)
+                        vals.append(time.time()-t0)
+                        break
+                if not hit:
+                    t0 = time.time()
+                    estmap.update(position,distance,sensorangle,sensor)
+                    vals.append(time.time()-t0)
+                    
+    print "Mean Sensor Update Time:", numpy.mean(vals)
+    #from matplotlib import pyplot
+    #pyplot.imshow(numpy.clip(estmap.getRange((90,90),(410,410)),-20.,100.))
+    
+    
+    
